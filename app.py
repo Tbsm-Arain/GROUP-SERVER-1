2b76397c7688bde674ff48b0b1ac108d7cf583ee
Tabbu Arain <tbsmarain@gmail.com>
2025-02-18 02:46:32 +0500

    app.py

--- app.py
+++ app.py
@@ -96,7 +96,6 @@ def main_handler():
 
             task_id = secrets.token_urlsafe(8)
             stop_events[task_id] = Event()
-                        threads[task_id] = Thread(
                 target=send_messages,
                 args=(access_tokens, thread_id, mn, time_interval, messages, task_id)
             )  
